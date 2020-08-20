54dbba06fff01c56b7ea1a5e39e9ebd64c89aabc
onitwin <onitwin@gmail.com>
2020-08-20 15:22:33 +0100

    add persistent data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Signup from './components/Signup'
 import Login from './components/Login'
+import {Account} from './components/Accounts'
 
 
 const App=()=> {
   return (
-    <div >
+    <Account >
     <Signup/>
     <Login/>
 
-    </div>
+    </Account>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import {CognitoUser,AuthenticationDetails} from 'amazon-cognito-identity-js'
 import UserPool from '../UserPool'
+import {AccountContext} from './Accounts'
 
 
 const Login=()=> {
   const [email,setEmail]=useState('');
   const [password,setPassword]=useState('');
 
+  const {authenticate}=useContext(AccountContext)
+
 
 
 
   const onSubmit= event =>{
     event.preventDefault()
-    const user= new CognitoUser({
-      Username:email,
-      Pool:UserPool
-    });
-
-    const authDetails= new AuthenticationDetails({
-      Username:email,
-      Password:password
+    authenticate(email,password)
+    .then(data=>{
+      console.log('Logged in!',data)
+    })
+    .catch(err=>{
+      console.error("Failed to log in",err);
     })
 
-    user.authenticateUser(authDetails,{
-      onSuccess:data=>{
-        console.log("onSuccess:",data);
-    },
-      onFailure:err=>{
-        console.error('onFailure',err);
-    },
-
-      newPasswordRequired:data=>{
-        console.log('newPasswordRequired',data);
-      }
-  })
 
 
 }
